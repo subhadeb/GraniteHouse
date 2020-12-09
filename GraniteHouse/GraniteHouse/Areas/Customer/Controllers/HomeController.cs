0d6bfdb71bd3a4528492d27c359e684298ab971c
Subha Deb <subdeb@deloitte.com>
2020-12-09 23:06:31 +0530

    After 292 Home Page  Details Remove Action Method

--- GraniteHouse/GraniteHouse/Areas/Customer/Controllers/HomeController.cs
+++ GraniteHouse/GraniteHouse/Areas/Customer/Controllers/HomeController.cs
@@ -45,6 +45,22 @@ namespace GraniteHouse.Controllers
             return RedirectToAction("Index", "Home", new { area = "Customer" });
 
         }
+        
+        public IActionResult Remove(int id)
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if (lstShoppingCart.Count > 0)
+            {
+                if (lstShoppingCart.Contains(id))
+                {
+                    lstShoppingCart.Remove(id);
+                }
+            }
+
+            HttpContext.Session.Set("ssShoppingCart", lstShoppingCart);
+
+            return RedirectToAction(nameof(Index));
+        }
 
 
 
