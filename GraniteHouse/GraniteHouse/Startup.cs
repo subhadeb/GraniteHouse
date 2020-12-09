6ddf525b786a7ac70ecf06fe29e66d4d827a2478
Subha Deb <subdeb@deloitte.com>
2020-12-09 09:35:42 +0530

    After 290 Home Page  Details Post Action Method

--- GraniteHouse/GraniteHouse/Startup.cs
+++ GraniteHouse/GraniteHouse/Startup.cs
@@ -30,7 +30,7 @@ namespace GraniteHouse
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -41,6 +41,12 @@ namespace GraniteHouse
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +68,7 @@ namespace GraniteHouse
             app.UseCookiePolicy();
 
             app.UseAuthentication();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
