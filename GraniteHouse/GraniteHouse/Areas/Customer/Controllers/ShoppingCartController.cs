76c6b2ccba9697c497cec99f8782328211ab171b
Subha Deb <subdeb@deloitte.com>
2020-12-12 09:22:33 +0530

    After 306 Shopping Cart  Appointment Confirmation View

--- GraniteHouse/GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
+++ GraniteHouse/GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -73,7 +73,7 @@ namespace GraniteHouse.Areas.Customer.Controllers
             lstCartItems = new List<int>();
             HttpContext.Session.Set("ssShoppingCart", lstCartItems);
 
-            return RedirectToAction("Index");
+            return RedirectToAction("AppointmentConfirmation", "ShoppingCart", new { Id = appointmentId });
 
         }
         public IActionResult Remove(int id)
@@ -91,6 +91,18 @@ namespace GraniteHouse.Areas.Customer.Controllers
             HttpContext.Session.Set("ssShoppingCart", lstCartItems);
 
             return RedirectToAction(nameof(Index));
+        }
+        public IActionResult AppointmentConfirmation(int id)
+        {
+            ShoppingCartVM.Appointments = _db.Appointments.Where(a => a.Id == id).FirstOrDefault();
+            List<ProductsSelectedForAppointment> objProdList = _db.ProductsSelectedForAppointment.Where(p => p.AppointmentId == id).ToList();
+
+            foreach (ProductsSelectedForAppointment prodAptObj in objProdList)
+            {
+                ShoppingCartVM.Products.Add(_db.Products.Include(p => p.ProductTypes).Include(p => p.SpecialTags).Where(p => p.Id == prodAptObj.ProductId).FirstOrDefault());
+            }
+
+            return View(ShoppingCartVM);
         }
     }
 }
